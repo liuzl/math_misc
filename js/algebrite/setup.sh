84c6caf5f8946e6c6687d169f5739123d106414a
Zhanliang Liu <zhanliangliu@qq.com>
2018-09-30 15:04:04 +0800

    Algebrite.js examples

---
+++ js/algebrite/setup.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/davidedc/Algebrite/master/dist/algebrite.js
+wget https://raw.githubusercontent.com/peterolson/BigInteger.js/master/BigInteger.js -O big-integer.js
